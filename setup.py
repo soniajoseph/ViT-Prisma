a7ec3687ffe1170e6a377a91cbbcf1b75d92571e
soniajoseph <soniamollyjoseph@gmail.com>
2023-10-02 15:28:39 -0400

    write setup file

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vit-planetarium',
+    version='0.1.0',
+    author='Sonia Joseph',
+    author_email='soniamollyljoseph@gmail.com',
+    description='A Vision Transformer library for mechanistic interpretability.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/vit-planetarium',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'torch>=1.8.0',
+        'torchvision>=0.9.0',
+        # Other dependencies...
+    ],
+    extras_require={
+        'dev': [
+            'pytest>=6.0',
+            # Other development dependencies...
+        ],
+    },
+    keywords='vision-transformer machine-learning',
+    zip_safe=False,
+)
