12c123ec21fca3960d5c9f757c69247be2be7ca2
soniajoseph <soniamollyjoseph@gmail.com>
2023-10-02 15:30:58 -0400

    update setup requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup, find_packages
 
+# Function to read the requirements file
+def read_requirements(file_path):
+    with open(file_path, 'r') as file:
+        return [line.strip() for line in file if line.strip() and not line.startswith('#')]
+
+# Read the requirements file
+install_requires = read_requirements('requirements.txt')
+
 setup(
     name='vit-planetarium',
     version='0.1.0',
     author='Sonia Joseph',
-    author_email='soniamollyljoseph@gmail.com',
+    author_email='soniamollyjoseph@gmail.com',
     description='A Vision Transformer library for mechanistic interpretability.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -19,11 +27,7 @@ setup(
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
     python_requires='>=3.6',
-    install_requires=[
-        'torch>=1.8.0',
-        'torchvision>=0.9.0',
-        # Other dependencies...
-    ],
+    install_requires=install_requires,
     extras_require={
         'dev': [
             'pytest>=6.0',
