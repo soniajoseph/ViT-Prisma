95c81c8e68e7625d996811a01761c888809d3355
Yash Vadi <yash.vadi@yahoo.com>
2023-10-25 16:51:33 -0400

    clean dsprites dataloader script
    Signed-off-by: Yash Vadi <yash.vadi@yahoo.com>

--- src/vit_prisma/dataloaders/dsprites.py
+++ src/vit_prisma/dataloaders/dsprites.py
@@ -1,27 +1,9 @@
 import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader, Subset
+from torch.utils.data import Dataset, Subset
 from sklearn.model_selection import train_test_split
 import os
 
-class DSpritesDataset(Dataset):
-    def __init__(self, data_path):
-        # Load the dataset
-        data = np.load(data_path, allow_pickle=True, encoding='latin1')
-        self.images = data['imgs']
-        self.latents_values = data['latents_values']
-        self.latents_classes = data['latents_classes']
-
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, idx):
-        # Convert image from numpy array to PyTorch tensor and add channel dimension
-        image = torch.tensor(self.images[idx], dtype=torch.float32).unsqueeze(0)
-        latents = torch.tensor(self.latents_values[idx], dtype=torch.float32)
-        classes = torch.tensor(self.latents_classes[idx], dtype=torch.int64)
-        return image, latents, classes
-    
 
 class DSpritesDataset(Dataset):
     def __init__(self, data_path):
