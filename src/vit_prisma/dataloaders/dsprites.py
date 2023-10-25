26fc42ab4a0885c7df848d2107101f4436d28f36
Yash Vadi <yash.vadi@yahoo.com>
2023-10-25 15:31:29 -0400

    Add dataloader for dSprites dataset
    Signed-off-by: Yash Vadi <yash.vadi@yahoo.com>

--- src/vit_prisma/dataloaders/dsprites.py
+++ src/vit_prisma/dataloaders/dsprites.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, Subset
+from sklearn.model_selection import train_test_split
 import os
 
 class DSpritesDataset(Dataset):
@@ -22,15 +23,33 @@ class DSpritesDataset(Dataset):
         return image, latents, classes
     
 
-data_path = '/network/scratch/s/sonia.joseph/datasets/dsprites.npz'
+class DSpritesDataset(Dataset):
+    def __init__(self, data_path):
+        # Load the dataset
+        data = np.load(data_path, allow_pickle=True, encoding='latin1')
 
-dsprites_dataset = DSpritesDataset(data_path)
-dataloader = DataLoader(dsprites_dataset, batch_size=32, shuffle=True)
+        self.images = data['imgs']
+        self.labels =  data['latents_values'][:,1]
 
-for images, latents_values, latents_classes in dataloader:
-    # Your training or evaluation code here
-    print(images.shape)
-    print(latents_values)
-    print(latents_classes)
+    def __len__(self):
+        return len(self.images)
 
-    break
+    def __getitem__(self, idx):
+        # Convert image from numpy array to PyTorch tensor and add channel dimension
+        image = torch.tensor(self.images[idx], dtype=torch.float32).unsqueeze(0)
+        label = torch.tensor(self.labels[idx]-1, dtype=torch.int64)
+        return image, label
+    
+
+def train_test_dataset(dataset, test_split=0.25):
+    train_idx, val_idx = train_test_split(list(range(len(dataset))), test_size=test_split)
+    datasets = {}
+    datasets['train'] = Subset(dataset, train_idx)
+    datasets['test'] = Subset(dataset, val_idx)
+    return datasets
+
+data_path = '/network/scratch/s/sonia.joseph/datasets/dsprites.npz' # path to the dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz data
+
+# Example to load the data with train and test split
+## dsprites_dataset = DSpritesDataset(data_path)
+## dsprites_datasets = train_test_dataset(dsprites_dataset, test_split=0.25)
