265c17759c89fcc9360ebf0cbfcde18318781363
soniajoseph <soniamollyjoseph@gmail.com>
2023-10-27 18:01:39 -0400

    start nb to visualize attention heads

---
+++ src/vit_prisma/utils/get_activations.py
@@ -0,0 +1,116 @@
+import torch
+from tqdm import tqdm
+import torch.nn as nn
+import numpy as np
+
+def get_activations(net,layer,data_loader,use_cuda=True, max_count=0, test_run=False, debug=True):
+    '''
+    Get activations of a layer for a given data_loader
+    '''
+    activations = []
+    def hook_fn(m,i,o):
+        activations.append(i[0].cpu().numpy())
+    handle = layer.register_forward_hook(hook_fn)
+    if use_cuda:
+        net = net.cuda()
+    net.eval()
+
+    count = 0
+    for i, (images, labels) in enumerate(tqdm(data_loader)):
+        # if debug: print(i)
+        if use_cuda:
+            images = images.cuda()
+        with torch.no_grad():
+            output = net(images)
+
+        count += 1
+        if count > max_count:
+            break
+    handle.remove()
+    activations_np = np.vstack(activations)     # assuming first dimension is num_examples: batches x batch_size x <feat_dims> --> num_examples x <feat_dims>
+    return activations_np
+
+class ActivationCacheHook:
+    '''
+    Hook to save activations of a layer
+    '''
+    def __init__(self):
+        self.activations = []
+
+    def __call__(self, module, input, output):
+        self.activations.append(output.clone())
+
+class CustomAttention(nn.Module):
+
+    def __init__(self, config, logger = None):
+        super().__init__()
+
+        self.logger = logger 
+        self.config = config
+
+        hidden_dim = self.config.transformer.hidden_dim
+
+        assert hidden_dim % self.config.transformer.num_heads == 0, "Embedding dimension must be divisible by number of heads"
+        self.head_dim = hidden_dim // self.config.transformer.num_heads
+        self.scale = self.head_dim ** -0.5
+        self.qkv = nn.Linear(hidden_dim, 3 * hidden_dim, bias=False)
+        self.q_norm, self.k_norm = nn.LayerNorm(self.head_dim) if self.config.layernorm.qknorm else nn.Identity(), nn.LayerNorm(self.head_dim) if self.config.layernorm.qknorm else nn.Identity()
+        self.attn_dropout = nn.Dropout(self.config.dropout.attention) if self.config.dropout.attention > 0 else nn.Identity()
+        self.proj = nn.Linear(hidden_dim, hidden_dim) if self.config.transformer.attn_hidden_layer else nn.Identity()
+        self.proj_dropout = nn.Dropout(self.config.dropout.proj) if self.config.dropout.proj > 0 else nn.Identity()
+        
+        self._log(f"Attention layer initialized with config {self.config}")
+
+        self.attn_scores = nn.Identity()
+        self.attn_pattern = nn.Identity()
+
+
+    def _log(self, msg):
+        if self.logger:
+            self.logger.info(msg)
+
+    def forward(self, x):
+        self._log(f"Attention input size is {x.shape}") 
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.config.transformer.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv.unbind(0)
+        q, k = self.q_norm(q), self.k_norm(k)
+        q = q * self.scale
+        attn = q @ k.transpose(-2, -1)
+        attention_scores = self.attn_scores(attn)
+        attn = attn.softmax(dim=-1)
+        attn_pattern  = self.attn_pattern(attn)
+        self._log(f"Attention size after softmax is {attn.shape}") 
+        attn = self.attn_dropout(attn)
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_dropout(x)
+        self._log(f"Attention output size is {x.shape}") 
+        return x
+
+def get_activations(net,layer,data_loader,use_cuda=True, max_count=0, test_run=False, debug=True):
+    '''
+    Get activations of a layer for a given data_loader
+    '''
+    activations = []
+    def hook_fn(m,i,o):
+        activations.append(i[0].cpu().numpy())
+    handle = layer.register_forward_hook(hook_fn)
+    if use_cuda:
+        net = net.cuda()
+    net.eval()
+
+    count = 0
+    for i, (images, labels) in enumerate(tqdm(data_loader)):
+        # if debug: print(i)
+        if use_cuda:
+            images = images.cuda()
+        with torch.no_grad():
+            output = net(images)
+
+        count += 1
+        if count > max_count:
+            break
+    handle.remove()
+    activations_np = np.vstack(activations)     # assuming first dimension is num_examples: batches x batch_size x <feat_dims> --> num_examples x <feat_dims>
+    return activations_np
--- src/vit_prisma/visualization/visualize_attention.py
+++ src/vit_prisma/visualization/visualize_attention.py
@@ -5,6 +5,136 @@ from IPython.core.display import display, HTML
 import string
 import random
 
+# Helper function to plot attention patterns, hide this function
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_attn_heads(total_activations, n_heads = 4, n_layers = 1, img_shape=32, idx=0, 
+    global_min_max=False, global_normalize=False, fourier_transform_local=False, fourier_transform_global=False, graph_type = "imshow_graph", cmap='viridis'):
+
+    log_transform = False
+    save_figure = False
+
+    total_data = np.zeros((n_layers, n_heads, img_shape, img_shape))
+    if global_min_max or global_normalize or fourier_transform_global:
+        for i in range(n_layers):
+            for j in range(n_heads):
+                data = total_activations[i][idx,j,:,:]
+                if log_transform:
+                    data = log10_stable(data)
+                if fourier_transform_global:
+                    data = np.abs(np.fft.fft2(data))
+
+                total_data[i,j,:,:] = data
+
+        total_min = np.min(total_data)
+        total_max = np.max(total_data)
+        print(f"Total Min: {total_min}, Total Max: {total_max}")
+
+        if global_normalize:
+            total_data = -1 + 2 * (total_data - total_min) / (total_max - total_min)
+            # put back in list
+            total_activations = []
+            for i in range(12):
+                matrix = np.expand_dims(total_data[idx,:,:,:], axis=0)
+                total_activations.append(matrix)
+
+    fig, axes = plt.subplots(n_layers, n_heads, figsize=(10, 5))
+    total_data_dict = {}
+
+    is_1d_axes = len(axes.shape) == 1
+
+
+    # Iterate over each row and column
+    for i in range(n_layers): # Layer
+
+        total_data_dict[f"Layer_{i}"] = {}
+
+        for j in range(n_heads): # Head
+
+            # Get Data
+            data = total_activations[i][0,j,:,:]
+
+            # Plot the imshow plot in the corresponding subplot
+            if graph_type == "histogram_graph":
+                data = data.flatten()
+                if log_transform:
+                    axes[i, j].set_yscale('log')
+                    axes[i, j].set_xscale('log')
+    #             axes[i, j].set_title(f"Layer {layer_num}, Head {head_num} Attention")
+                n, bins, patches = axes[i, j].hist(data, bins=100)
+
+            elif graph_type == "imshow_graph": # Plot imshow
+
+                if log_transform:
+                    data = log10_stable(data)
+                    data = np.round(data, 5)
+                if fourier_transform_local:
+                    data = np.abs(np.fft.fft2(data))
+
+                if global_min_max or fourier_transform_global:
+                    vmin = total_min
+                    vmax = total_max
+                elif global_normalize:
+                    vmin = -1
+                    vmax = 1
+                else:
+                    vmax = np.max(data)
+                    vmin = np.min(data)
+                
+                if is_1d_axes:
+                    im = axes[j].imshow(np.round(data,5), vmin=vmin, vmax=vmax, cmap=cmap)
+                    axes[j].axis('off')
+                else:
+                    im = axes[i, j].imshow(np.round(data,5), vmin=vmin, vmax=vmax, cmap=cmap)
+                    axes[i, j].axis('off')
+                    
+                total_data_dict[f"Layer_{i}"][f"Head_{j}"] = [[round(num, 5) for num in row] for row in data.tolist()]
+
+                # Remove axis labels and ticks
+            if i == 0:
+                if is_1d_axes:
+                    axes[j].set_title(f"Head {j}", fontsize=12, pad=5)
+                else:
+                    axes[i, j].set_title(f"Head {j}", fontsize=12, pad=5)
+            if j == 0:
+                if is_1d_axes:
+                    axes[j].text(-0.3, 0.5, f"Layer {i}", fontsize=12, rotation=90, ha='center', va='center', transform=axes[j].transAxes)
+                else:
+                    axes[i, j].text(-0.3, 0.5, f"Layer {i}", fontsize=12, rotation=90, ha='center', va='center', transform=axes[i, j].transAxes)
+
+    # Add colorbar
+    if graph_type == "imshow_graph" and global_min_max:
+        cbar_ax = fig.add_axes([0.15, 0.95, 0.7, 0.01])
+        # Create a colorbar in the new axes
+        cbar = fig.colorbar(im, cax=cbar_ax, orientation='horizontal', shrink=.4)
+        if global_min_max:
+            cbar.set_label('Color Scale Across All Attention Heads', fontsize=16)
+        else:
+            cbar.set_label('Color Scale for Each Individual Attention Head', fontsize=16)
+
+    # Adjust the spacing between subplots
+    if graph_type == "histogram_graph":
+        plt.subplots_adjust(wspace=0.6, hspace=0.6)
+    elif graph_type == "imshow_graph":
+        plt.subplots_adjust(wspace=0.1, hspace=0.2)
+
+    # if global_min_max:
+    #     plt.suptitle(f'Attention Scores for ViT on CIFAR-10 for Image Idx {idx}', fontsize=20, y=.98)
+    # else:
+    #     plt.suptitle(f'Attention Scores for ViT on CIFAR-10 for Image Idx {idx} (Colorbar per Image, not Global)', fontsize=20, y=.98)
+
+    # plt.tight_layout()
+    # save_path = './figures/attn_pattern_imshow_log_globalcolor.png'
+    # save_path = './figures/attn_pattern_imshow_log.png'
+    # if save_figure:
+    #     save_path = './figures/attn_scores_truck_imshow_log_globalcolor.png'
+    #     plt.savefig(save_path, bbox_inches='tight')
+    #     print(f"Saved at {save_path}")
+
+    plt.show()
+
 def generate_random_string(length=10):
     '''
     Helper function to generate canvas IDs for javascript figures.
