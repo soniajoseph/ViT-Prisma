62823c064b0ca39b7b575353864efb47b5ea6550
Praneet <praneetquantumx@gmail.com>
2023-10-29 18:55:02 +1100

    Support for pretrained models from timm along with Huggingface

--- src/vit_prisma/models/pretrained_model.py
+++ src/vit_prisma/models/pretrained_model.py
@@ -1,19 +1,75 @@
 import torch.nn as nn
-from transformers import ViTModel, AutoImageProcessor
+from transformers import ViTForImageClassification, AutoImageProcessor
+import timm
 
 
 class PretrainedModel(nn.Module):
-    def __init__(self, pretrained_model: str, config):
+    '''
+    Wrapper class for pretrained models from huggingface and timm, 
+    which can be used for finetuning with the trainer available in the Prisma project.
+    '''
+    def __init__(self, pretrained_model_name: str, config, is_timm: bool = False):
         super().__init__()
-        self.pretrained_model = pretrained_model
-        self.image_processor = AutoImageProcessor.from_pretrained(pretrained_model)
-        self.n_classes = config.classification.num_classes
-        self.net = ViTModel.from_pretrained(pretrained_model)
-        self.classifier = nn.Linear(self.net.config.hidden_size, self.n_classes)
+        self.is_timm = is_timm
         
+        self.pretrained_model_name = pretrained_model_name
+        self.n_classes = None if config is None else config.classification.num_classes
+        if is_timm:
+            self.model = timm.create_model(pretrained_model_name, pretrained=True, num_classes=self.n_classes)
+        else:
+            self.image_processor = AutoImageProcessor.from_pretrained(pretrained_model_name)
+            self.model = ViTForImageClassification.from_pretrained(pretrained_model_name)
+
+            self.vit = self.model.vit
+
+            if config is None or self.n_classes == self.model.classifier.out_features:
+                self.classifier = self.model.classifier
+            else:
+                print(f"Initializing new classifier head for finetuning with shape: ({self.model.classifier.in_features, self.n_classes})")
+                self.classifier = nn.Linear(self.model.classifier.in_features, self.n_classes)
 
     def forward(self, x):
-        x = self.image_processor(x, return_tensors="pt").pixel_values
-        x = self.net(x).last_hidden_state[:, 0, :]
-        x = self.classifier(x)
+        if self.is_timm:
+            x = self.model(x)
+        else:
+            x = self.image_processor(x, return_tensors="pt").pixel_values
+            x = self.vit(x).last_hidden_state[:, 0, :]
+            x = self.classifier(x)
+        
         return x
+    
+    def convert_hf_model_config(self):
+        if self.is_timm:
+            raise NotImplementedError
+        else:
+            hf_config = self.vit.config
+            prisma_config = {
+                'image_config': {
+                    'image_size': hf_config.image_size,
+                    'patch_size': hf_config.patch_size,
+                    'n_channels': hf_config.num_channels,
+                },
+                'transformer_config': {
+                    'hidden_dim': hf_config.hidden_size,
+                    'num_heads': hf_config.num_attention_heads,
+                    'num_layers': hf_config.num_hidden_layers,
+                    'mlp_dim': hf_config.intermediate_size,
+                    'activation_name': hf_config.hidden_act,
+                },
+                'layer_norm_config': {
+                    'qknorm': None,
+                    'layer_norm_eps': hf_config.layer_norm_eps,
+                },
+                'dropout_config': {
+                    'patch': hf_config.hidden_dropout_prob,
+                    'position': hf_config.hidden_dropout_prob,
+                    'attention': hf_config.attention_probs_dropout_prob,
+                    'proj': None,
+                    'mlp': None,
+                },
+                'classification_config': {
+                    'num_classes': self.classifier.out_features,
+                },
+            }
+
+            return prisma_config
