9bf6bd642677ca7ab5c27cf3f0dff62d61976d29
Rob <robbie.g.33@gmail.com>
2024-01-16 16:35:19 -0500

    Changed the visualization tool to take in multiple images and mutiple sizes. Also an option to provide names. Cleaned up code a bit

--- src/vit_prisma/visualization/main_visualize.js
+++ src/vit_prisma/visualization/main_visualize.js
@@ -11,41 +11,14 @@ if (CLS_TOKEN) {
     var clsTokenOffset = 0;
 }
 
-var matrixColorsImg = Array(NUM_PATCH_WIDTH).fill().map(() => Array(NUM_PATCH_WIDTH).fill('')); // cifar image
-var matrixColorsAttn = Array(Math.pow(NUM_PATCHES, 2)).fill().map(() => Array(Math.pow(NUM_PATCHES, 2)).fill('')); // attention head
 
-// PLOT image on canvasImg
-var patches = JSON.parse(PATCHES_JSON);
-var canvasImg = document.getElementById(CANVAS_IMG_ID);
-var ctxImg = canvasImg.getContext('2d');
-var idx = 0;
-for (let i = 0; i < IMAGE_SIZE; i+= PATCH_SIZE) {
-    for (let j = 0; j < IMAGE_SIZE; j += PATCH_SIZE) {
-        var imgData = ctxImg.createImageData(PATCH_SIZE, PATCH_SIZE);
-        var data = imgData.data;
-        var patch = patches[idx];
 
-        for (let p = 0, q = 0; p < patch.length; p += 3, q += 4) {
-            data[q] = patch[p];
-            data[q + 1] = patch[p + 1];
-            data[q + 2] = patch[p + 2];
-            data[q + 3] = 255;
 
-        }
-        const row = Math.floor(i / PATCH_SIZE);
-        const col = Math.floor(j / PATCH_SIZE);
-
-        // Storing the representative color for this patch.
-        // You can use the first pixel as a representative color, or calculate the average color of the patch.
-        matrixColorsImg[row][col] = patch
-
-        ctxImg.putImageData(imgData, j, i);
-        ctxImg.strokeStyle = 'white';
-        ctxImg.strokeRect(j, i, PATCH_SIZE, PATCH_SIZE);
-
-        idx++;
-    }
-}
+var matrixColorsAttn;  // attention head
+var matrixColorsImg;  // image
+var numPatches;
+var patchSize;
+var numPatchWidth;
 
 // Plot attention head on canvasAttn
 var attnHead = JSON.parse(ATTN_HEAD_JSON);
@@ -56,8 +29,14 @@ var attnHeadSelector = document.getElementById(ATTN_HEAD_SELECTOR_ID)
 
 // initialize a new attn head
 function setNewAttnHead(currentAttnIndex){
-    for (let i = 0; i < NUM_PATCHES; i++) {
-        for (let j = 0; j < NUM_PATCHES; j++) {
+    numPatches = attnHead[currentAttnIndex].length;
+    canvasAttn.width = numPatches * ATTN_SCALING;
+    canvasAttn.height = numPatches * ATTN_SCALING;
+    canvasAttn.style.width = numPatches * ATTN_SCALING + "px";
+    canvasAttn.style.height = numPatches * ATTN_SCALING + "px";
+    matrixColorsAttn = Array(Math.pow(numPatches, 2)).fill().map(() => Array(Math.pow(numPatches, 2)).fill(''));
+    for (let i = 0; i < numPatches; i++) {
+        for (let j = 0; j < numPatches; j++) {
             var color = getColor(attnHead[currentAttnIndex][i][j]);
             ctxAttn.fillStyle = color;
             ctxAttn.fillRect(j * ATTN_SCALING, i * ATTN_SCALING, ATTN_SCALING, ATTN_SCALING);
@@ -68,12 +47,62 @@ function setNewAttnHead(currentAttnIndex){
 
 setNewAttnHead(0);
 
+var patches = JSON.parse(PATCHES_JSON);
+var canvasImg = document.getElementById(CANVAS_IMG_ID);
+var ctxImg = canvasImg.getContext('2d');
+var imageSizes = JSON.parse(IMAGE_SIZES_JSON);
+
+// initialize a new image
+function setNewImage(currentImageIndex){
+    var imageSize = imageSizes[currentImageIndex];
+
+    canvasImg.width = imageSize;
+    canvasImg.height = imageSize;
+    canvasImg.style.width = (imageSize + 20) + "px";
+    canvasImg.style.height = (imageSize + 20) + "px";
+
+    patchSize = Math.floor(imageSize / Math.sqrt(numPatches - 1));
+    numPatchWidth = Math.floor(imageSize / (patchSize - 1))
+    matrixColorsImg = Array(numPatchWidth).fill().map(() => Array(numPatchWidth).fill(''));
+    var idx = 0;
+    for (let i = 0; i < imageSize; i+= patchSize) {
+        for (let j = 0; j < imageSize; j += patchSize) {
+            var imgData = ctxImg.createImageData(patchSize, patchSize);
+            var data = imgData.data;
+            var patch = patches[currentImageIndex][idx];
+
+            for (let p = 0, q = 0; p < patch.length; p += 3, q += 4) {
+                data[q] = patch[p];
+                data[q + 1] = patch[p + 1];
+                data[q + 2] = patch[p + 2];
+                data[q + 3] = 255;
+
+            }
+            const row = Math.floor(i / patchSize);
+            const col = Math.floor(j / patchSize);
+
+            // Storing the representative color for this patch.
+            // You can use the first pixel as a representative color, or calculate the average color of the patch.
+            matrixColorsImg[row][col] = patch
+
+            ctxImg.putImageData(imgData, j, i);
+            ctxImg.strokeStyle = 'white';
+            ctxImg.strokeRect(j, i, patchSize, patchSize);
+
+            idx++;
+        }
+    }
+}
+setNewImage(0);
+
+var names = JSON.parse(NAMES_JSON);
+
 //// put options in the dropdown box
 function populateAttnHeadSelector() {
         attnHead.forEach((_, index) => {
             var option = document.createElement('option');
             option.value = index;
-            option.text = 'Attention Head ' + (index + 1);
+            option.text = (index + 1) + ": " + names[index];
             attnHeadSelector.appendChild(option);
         });
     }
@@ -84,18 +113,18 @@ function redrawImgPatch(col, row) {
         if (matrixColorsImg[row] && matrixColorsImg[row][col]){
 
         var originalPatch = matrixColorsImg[row][col];
-        var imgData = patchToImageData(originalPatch, PATCH_SIZE, PATCH_SIZE);
+        var imgData = patchToImageData(originalPatch, patchSize, patchSize);
 
-        ctxImg.putImageData(imgData, col * PATCH_SIZE, row * PATCH_SIZE);
+        ctxImg.putImageData(imgData, col * patchSize, row * patchSize);
         ctxImg.strokeStyle = 'white';
-        ctxImg.strokeRect(col*PATCH_SIZE, row*PATCH_SIZE, PATCH_SIZE, PATCH_SIZE);
+        ctxImg.strokeRect(col*patchSize, row*patchSize, patchSize, patchSize);
 
         }
 }
 
 function redrawImg(){
-   for (let r = 0; r < NUM_PATCH_WIDTH; r+= 1) {
-        for (let c = 0; c < NUM_PATCH_WIDTH; c += 1) {
+   for (let r = 0; r < numPatchWidth; r+= 1) {
+        for (let c = 0; c < numPatchWidth; c += 1) {
 
             redrawImgPatch(c, r);
         }
@@ -111,7 +140,7 @@ function redrawAttnEntry(x,y){
 
 // convert row OR col of attention matrix to the corresponding patch row and col
 function attnPos2ImgRowCol(x){
-    return [Math.floor((x - clsTokenOffset) / NUM_PATCH_WIDTH), (x - clsTokenOffset) % NUM_PATCH_WIDTH];
+    return [Math.floor((x - clsTokenOffset) / numPatchWidth), (x - clsTokenOffset) % numPatchWidth];
 }
 
 // redraw everything drawn when x and y are highlighted
@@ -145,8 +174,8 @@ function redrawAll(x,y){
 canvasAttn.addEventListener('mousemove', function(event) {
 
     // find x,y + corresponding img row/cols for the current mouse position
-    if (event.offsetY == ATTN_SCALING*NUM_PATCHES){
-        var x = NUM_PATCHES -1;
+    if (event.offsetY == ATTN_SCALING*numPatches){
+        var x = numPatches -1;
     }
     else{
         var x = Math.floor(event.offsetY / ATTN_SCALING);
@@ -154,9 +183,9 @@ canvasAttn.addEventListener('mousemove', function(event) {
 
     const rowColImg = attnPos2ImgRowCol(x);
 
-    if (event.offsetX == ATTN_SCALING*NUM_PATCHES){
+    if (event.offsetX == ATTN_SCALING*numPatches){
 
-        var y = NUM_PATCHES -1;
+        var y = numPatches -1;
     }
     else{
         var y = Math.floor(event.offsetX / ATTN_SCALING);
@@ -173,32 +202,32 @@ canvasAttn.addEventListener('mousemove', function(event) {
 
         if (!(CLS_TOKEN&&(x==0))){
             ctxImg.fillStyle = colorTokenA;
-            ctxImg.fillRect(rowColImg[1] * PATCH_SIZE,  rowColImg[0] * PATCH_SIZE, PATCH_SIZE, PATCH_SIZE);
+            ctxImg.fillRect(rowColImg[1] * patchSize,  rowColImg[0] * patchSize, patchSize, patchSize);
         }
         else{
             // CLS Token selected for x, highlight all
             ctxImg.fillStyle = colorTokenA;
 
-          for (let r = 0; r < NUM_PATCH_WIDTH; r+= 1) {
-                for (let c = 0; c < NUM_PATCH_WIDTH; c += 1) {
+          for (let r = 0; r < numPatchWidth; r+= 1) {
+                for (let c = 0; c < numPatchWidth; c += 1) {
 
-                    ctxImg.fillRect(c * PATCH_SIZE,  r * PATCH_SIZE, PATCH_SIZE, PATCH_SIZE);
+                    ctxImg.fillRect(c * patchSize,  r * patchSize, patchSize, patchSize);
                 }
 
             }
         }
         if (!(CLS_TOKEN&&(y==0))){
             ctxImg.fillStyle = colorTokenB;
-            ctxImg.fillRect(rowColImgSecond[1] * PATCH_SIZE, rowColImgSecond[0] * PATCH_SIZE, PATCH_SIZE, PATCH_SIZE);  // Second highlighted pixel
+            ctxImg.fillRect(rowColImgSecond[1] * patchSize, rowColImgSecond[0] * patchSize, patchSize, patchSize);  // Second highlighted pixel
         }
         else{
             // CLS Token selected for y, highlight all
             ctxImg.fillStyle = colorTokenB;
 
-           for (let r = 0; r < NUM_PATCH_WIDTH; r+= 1) {
-                for (let c = 0; c < NUM_PATCH_WIDTH; c += 1) {
+           for (let r = 0; r < numPatchWidth; r+= 1) {
+                for (let c = 0; c < numPatchWidth; c += 1) {
 
-                    ctxImg.fillRect(c * PATCH_SIZE,  r * PATCH_SIZE, PATCH_SIZE, PATCH_SIZE);
+                    ctxImg.fillRect(c * patchSize,  r * patchSize, patchSize, patchSize);
                 }
            }
         }
@@ -231,6 +260,7 @@ canvasAttn.addEventListener('mouseout', function() {
 attnHeadSelector.addEventListener('change', function() {
     var selectedIndex = parseInt(this.value);
     setNewAttnHead(selectedIndex);
+    setNewImage(selectedIndex);
     lastHighlightedX = null;
     lastHighlightedY = null;
 });
