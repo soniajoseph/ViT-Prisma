7f434e64cf5af14ebf1e216fd1d233a5bdb3828d
soniajoseph <soniamollyjoseph@gmail.com>
2024-01-28 13:01:59 -0500

    start hooked mlp

--- src/vit_prisma/models/layers/mlp.py
+++ src/vit_prisma/models/layers/mlp.py
@@ -3,39 +3,41 @@ import torch.nn as nn
 
 class MLP(nn.Module):
 
-    def __init__(self, config, 
-    logger=None):
-        super().__init__()
-        self.logger = logger
-        self.config = config
+    
 
-        hidden_dim = self.config.transformer.hidden_dim
-        mlp_dim = self.config.transformer.mlp_dim
+    # def __init__(self, config, 
+    # logger=None):
+    #     super().__init__()
+    #     self.logger = logger
+    #     self.config = config
 
-        self.fc1 = nn.Linear(hidden_dim, mlp_dim)
-        self.dropout1 = nn.Dropout(self.config.dropout.mlp) if self.config.dropout.mlp > 0 else nn.Identity()
-        self.act_fn = self.config.transformer.activation_fn()
-        self.mlp_norm = nn.LayerNorm(mlp_dim, eps=self.config.layernorm.layer_norm_eps) if self.config.layernorm.layer_norm_eps > 0 else nn.Identity()
-        self.fc2 = nn.Linear(mlp_dim, hidden_dim)
-        self.dropout2 = nn.Dropout(self.config.dropout.mlp) if self.config.dropout.mlp > 0 else nn.Identity()
+    #     hidden_dim = self.config.transformer.hidden_dim
+    #     mlp_dim = self.config.transformer.mlp_dim
 
-    def _log(self, msg, tensor):
-        if self.logger:
-            self.logger.info(f"{msg} size: {tensor.shape}")
+    #     self.fc1 = nn.Linear(hidden_dim, mlp_dim)
+    #     self.dropout1 = nn.Dropout(self.config.dropout.mlp) if self.config.dropout.mlp > 0 else nn.Identity()
+    #     self.act_fn = self.config.transformer.activation_fn()
+    #     self.mlp_norm = nn.LayerNorm(mlp_dim, eps=self.config.layernorm.layer_norm_eps) if self.config.layernorm.layer_norm_eps > 0 else nn.Identity()
+    #     self.fc2 = nn.Linear(mlp_dim, hidden_dim)
+    #     self.dropout2 = nn.Dropout(self.config.dropout.mlp) if self.config.dropout.mlp > 0 else nn.Identity()
 
-    def forward(self, x):
-        self._log("MLP input", x)
+    # def _log(self, msg, tensor):
+    #     if self.logger:
+    #         self.logger.info(f"{msg} size: {tensor.shape}")
 
-        x = self.fc1(x)
-        x = self.act_fn(x)
-        x = self.dropout1(x)
+    # def forward(self, x):
+    #     self._log("MLP input", x)
+
+    #     x = self.fc1(x)
+    #     x = self.act_fn(x)
+    #     x = self.dropout1(x)
         
-        self._log("MLP after first FC", x)
+    #     self._log("MLP after first FC", x)
 
-        x = self.mlp_norm(x)
-        x = self.fc2(x)
-        x = self.dropout2(x)
+    #     x = self.mlp_norm(x)
+    #     x = self.fc2(x)
+    #     x = self.dropout2(x)
 
-        self._log("MLP output", x)
+    #     self._log("MLP output", x)
 
-        return x
+    #     return x
