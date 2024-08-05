c0e06592decbe4a13e25d72d7c55583d041109db
soniajoseph <soniamollyjoseph@gmail.com>
2024-08-04 20:02:20 -0400

    finish off config

--- src/vit_prisma/sae/config.py
+++ src/vit_prisma/sae/config.py
@@ -26,7 +26,9 @@ DTYPE_MAP = {
 HfDataset = DatasetDict | Dataset | IterableDatasetDict | IterableDataset
 
 @dataclass
-class VisionModelRunnerConfig():
+class VisionModelSAERunnerConfig:
+    
+    # Sonia Note: make sure that everything here is essential for vision models, group together hyperparams that are vision-specific
     """
     Configuration for training a sparse autoencoder on a vision model.
 
@@ -111,6 +113,315 @@ class VisionModelRunnerConfig():
         model_from_pretrained_kwargs (dict[str, Any]): Additional keyword arguments for the model from pretrained.
     """
 
-    # Vision-specific parameters
+    # VISION-SPECIFIC HYPERPARAMETERS
     store_batch_size: int = 32 # num images ## Sonia: ?? Can we make this beter-named? 
     l1_loss_wd_norm: bool = False # Not sure what this means either
+
+    # Data Generating Function (Model + Training Distibuion)
+    model_name: str = "gelu-2l"
+    model_class_name: str = "HookedViT"
+    hook_name: str = "blocks.0.hook_mlp_out"
+    hook_eval: str = "NOT_IN_USE"
+    hook_layer: int = 0
+    hook_head_index: Optional[int] = None
+    dataset_path: str = ""
+    dataset_trust_remote_code: bool = True
+    streaming: bool = True
+    is_dataset_tokenized: bool = True
+    context_size: int = 128 # this is the patch_size
+    use_cached_activations: bool = False
+    cached_activations_path: Optional[str] = (
+        None  # Defaults to "activations/{dataset}/{model}/{full_hook_name}_{hook_head_index}"
+    )
+
+    # SAE Parameters
+    architecture: Literal["standard", "gated"] = "standard"
+    d_in: int = 512
+    d_sae: Optional[int] = None
+    b_dec_init_method: str = "geometric_median"
+    expansion_factor: int = 4
+    activation_fn: str = "relu"  # relu, tanh-relu, topk
+    activation_fn_kwargs: dict[str, Any] = field(default_factory=dict)  # for topk
+    normalize_sae_decoder: bool = True
+    noise_scale: float = 0.0
+    from_pretrained_path: Optional[str] = None
+    apply_b_dec_to_input: bool = True
+    decoder_orthogonal_init: bool = False
+    decoder_heuristic_init: bool = False
+    init_encoder_as_decoder_transpose: bool = False
+
+    # Activation Store Parameters
+    n_batches_in_buffer: int = 20
+    training_tokens: int = 2_000_000
+    finetuning_tokens: int = 0
+    store_batch_size_prompts: int = 32
+    train_batch_size_tokens: int = 4096
+    normalize_activations: str = (
+        "none"  # none, expected_average_only_in (Anthropic April Update), constant_norm_rescale (Anthropic Feb Update)
+    )
+
+    # Misc
+    device: str = "cpu"
+    act_store_device: str = "with_model"  # will be set by post init if with_model
+    seed: int = 42
+    dtype: str = "float32"  # type: ignore #
+    prepend_bos: bool = True
+
+    # Performance - see compilation section of lm_runner.py for info
+    autocast: bool = False  # autocast to autocast_dtype during training
+    autocast_lm: bool = False  # autocast lm during activation fetching
+    compile_llm: bool = False  # use torch.compile on the LLM
+    llm_compilation_mode: str | None = None  # which torch.compile mode to use
+    compile_sae: bool = False  # use torch.compile on the SAE
+    sae_compilation_mode: str | None = None
+
+    # Training Parameters
+
+    ## Batch size
+    train_batch_size_tokens: int = 4096
+
+    ## Adam
+    adam_beta1: float = 0
+    adam_beta2: float = 0.999
+
+    ## Loss Function
+    mse_loss_normalization: Optional[str] = None
+    l1_coefficient: float = 1e-3
+    lp_norm: float = 1
+    scale_sparsity_penalty_by_decoder_norm: bool = False
+    l1_warm_up_steps: int = 0
+
+    ## Learning Rate Schedule
+    lr: float = 3e-4
+    lr_scheduler_name: str = (
+        "constant"  # constant, cosineannealing, cosineannealingwarmrestarts
+    )
+    lr_warm_up_steps: int = 0
+    lr_end: Optional[float] = None  # only used for cosine annealing, default is lr / 10
+    lr_decay_steps: int = 0
+    n_restart_cycles: int = 1  # used only for cosineannealingwarmrestarts
+
+    ## FineTuning
+    finetuning_method: Optional[str] = None  # scale, decoder or unrotated_decoder
+
+    # Resampling protocol args
+    use_ghost_grads: bool = False  # want to change this to true on some timeline.
+    feature_sampling_window: int = 2000
+    dead_feature_window: int = 1000  # unless this window is larger feature sampling,
+
+    dead_feature_threshold: float = 1e-8
+
+    # Evals
+    n_eval_batches: int = 10
+    eval_batch_size_prompts: int | None = None  # useful if evals cause OOM
+
+    # WANDB
+    log_to_wandb: bool = True
+    log_activations_store_to_wandb: bool = False
+    log_optimizer_state_to_wandb: bool = False
+    wandb_project: str = "mats_sae_training_language_model"
+    wandb_id: Optional[str] = None
+    run_name: Optional[str] = None
+    wandb_entity: Optional[str] = None
+    wandb_log_frequency: int = 10
+    eval_every_n_wandb_logs: int = 100  # logs every 1000 steps.
+
+    # Misc
+    resume: bool = False
+    n_checkpoints: int = 0
+    checkpoint_path: str = "checkpoints"
+    verbose: bool = True
+    model_kwargs: dict[str, Any] = field(default_factory=dict)
+    model_from_pretrained_kwargs: dict[str, Any] = field(default_factory=dict)
+    sae_lens_version: str = field(default_factory=lambda: __version__)
+    sae_lens_training_version: str = field(default_factory=lambda: __version__)
+
+    def __post_init__(self):
+
+        if self.resume:
+            raise ValueError(
+                "Resuming is no longer supported. You can finetune a trained SAE using cfg.from_pretrained path."
+                + "If you want to load an SAE with resume=True in the config, please manually set resume=False in that config."
+            )
+
+        if self.use_cached_activations and self.cached_activations_path is None:
+            self.cached_activations_path = _default_cached_activations_path(
+                self.dataset_path,
+                self.model_name,
+                self.hook_name,
+                self.hook_head_index,
+            )
+
+        if not isinstance(self.expansion_factor, list):
+            self.d_sae = self.d_in * self.expansion_factor
+        self.tokens_per_buffer = (
+            self.train_batch_size_tokens * self.context_size * self.n_batches_in_buffer
+        )
+
+        if self.run_name is None:
+            self.run_name = f"{self.d_sae}-L1-{self.l1_coefficient}-LR-{self.lr}-Tokens-{self.training_tokens:3.3e}"
+
+        if self.b_dec_init_method not in ["geometric_median", "mean", "zeros"]:
+            raise ValueError(
+                f"b_dec_init_method must be geometric_median, mean, or zeros. Got {self.b_dec_init_method}"
+            )
+
+        if self.normalize_sae_decoder and self.decoder_heuristic_init:
+            raise ValueError(
+                "You can't normalize the decoder and use heuristic initialization."
+            )
+
+        if self.normalize_sae_decoder and self.scale_sparsity_penalty_by_decoder_norm:
+            raise ValueError(
+                "Weighting loss by decoder norm makes no sense if you are normalizing the decoder weight norms to 1"
+            )
+
+        # if we use decoder fine tuning, we can't be applying b_dec to the input
+        if (self.finetuning_method == "decoder") and (self.apply_b_dec_to_input):
+            raise ValueError(
+                "If we are fine tuning the decoder, we can't be applying b_dec to the input.\nSet apply_b_dec_to_input to False."
+            )
+
+        if self.normalize_activations not in [
+            "none",
+            "expected_average_only_in",
+            "constant_norm_rescale",
+            "layer_norm",
+        ]:
+            raise ValueError(
+                f"normalize_activations must be none, expected_average_only_in, or constant_norm_rescale. Got {self.normalize_activations}"
+            )
+
+        if self.act_store_device == "with_model":
+            self.act_store_device = self.device
+
+        if self.lr_end is None:
+            self.lr_end = self.lr / 10
+
+        unique_id = self.wandb_id
+        if unique_id is None:
+            unique_id = cast(
+                Any, wandb
+            ).util.generate_id()  # not sure why this type is erroring
+        self.checkpoint_path = f"{self.checkpoint_path}/{unique_id}"
+
+        if self.verbose:
+            print(
+                f"Run name: {self.d_sae}-L1-{self.l1_coefficient}-LR-{self.lr}-Tokens-{self.training_tokens:3.3e}"
+            )
+            # Print out some useful info:
+            n_tokens_per_buffer = (
+                self.store_batch_size_prompts
+                * self.context_size
+                * self.n_batches_in_buffer
+            )
+            print(f"n_tokens_per_buffer (millions): {n_tokens_per_buffer / 10 ** 6}")
+            n_contexts_per_buffer = (
+                self.store_batch_size_prompts * self.n_batches_in_buffer
+            )
+            print(
+                f"Lower bound: n_contexts_per_buffer (millions): {n_contexts_per_buffer / 10 ** 6}"
+            )
+
+            total_training_steps = (
+                self.training_tokens + self.finetuning_tokens
+            ) // self.train_batch_size_tokens
+            print(f"Total training steps: {total_training_steps}")
+
+            total_wandb_updates = total_training_steps // self.wandb_log_frequency
+            print(f"Total wandb updates: {total_wandb_updates}")
+
+            # how many times will we sample dead neurons?
+            # assert self.dead_feature_window <= self.feature_sampling_window, "dead_feature_window must be smaller than feature_sampling_window"
+            n_feature_window_samples = (
+                total_training_steps // self.feature_sampling_window
+            )
+            print(
+                f"n_tokens_per_feature_sampling_window (millions): {(self.feature_sampling_window * self.context_size * self.train_batch_size_tokens) / 10 ** 6}"
+            )
+            print(
+                f"n_tokens_per_dead_feature_window (millions): {(self.dead_feature_window * self.context_size * self.train_batch_size_tokens) / 10 ** 6}"
+            )
+            print(
+                f"We will reset the sparsity calculation {n_feature_window_samples} times."
+            )
+            # print("Number tokens in dead feature calculation window: ", self.dead_feature_window * self.train_batch_size_tokens)
+            print(
+                f"Number tokens in sparsity calculation window: {self.feature_sampling_window * self.train_batch_size_tokens:.2e}"
+            )
+
+        if self.use_ghost_grads:
+            print("Using Ghost Grads.")
+
+    @property
+    def total_training_tokens(self) -> int:
+        return self.training_tokens + self.finetuning_tokens
+
+    @property
+    def total_training_steps(self) -> int:
+        return self.total_training_tokens // self.train_batch_size_tokens
+
+    def get_base_sae_cfg_dict(self) -> dict[str, Any]:
+        return {
+            # TEMP
+            "architecture": self.architecture,
+            "d_in": self.d_in,
+            "d_sae": self.d_sae,
+            "dtype": self.dtype,
+            "device": self.device,
+            "model_name": self.model_name,
+            "hook_name": self.hook_name,
+            "hook_layer": self.hook_layer,
+            "hook_head_index": self.hook_head_index,
+            "activation_fn_str": self.activation_fn,
+            "apply_b_dec_to_input": self.apply_b_dec_to_input,
+            "context_size": self.context_size,
+            "prepend_bos": self.prepend_bos,
+            "dataset_path": self.dataset_path,
+            "dataset_trust_remote_code": self.dataset_trust_remote_code,
+            "finetuning_scaling_factor": self.finetuning_method is not None,
+            "sae_lens_training_version": self.sae_lens_training_version,
+            "normalize_activations": self.normalize_activations,
+            "activation_fn_kwargs": self.activation_fn_kwargs,
+        }
+
+    def get_training_sae_cfg_dict(self) -> dict[str, Any]:
+        return {
+            **self.get_base_sae_cfg_dict(),
+            "l1_coefficient": self.l1_coefficient,
+            "lp_norm": self.lp_norm,
+            "use_ghost_grads": self.use_ghost_grads,
+            "normalize_sae_decoder": self.normalize_sae_decoder,
+            "noise_scale": self.noise_scale,
+            "decoder_orthogonal_init": self.decoder_orthogonal_init,
+            "mse_loss_normalization": self.mse_loss_normalization,
+            "decoder_heuristic_init": self.decoder_heuristic_init,
+            "init_encoder_as_decoder_transpose": self.init_encoder_as_decoder_transpose,
+            "normalize_activations": self.normalize_activations,
+        }
+
+    def to_dict(self) -> dict[str, Any]:
+
+        cfg_dict = {
+            **self.__dict__,
+            # some args may not be serializable by default
+            "dtype": str(self.dtype),
+            "device": str(self.device),
+            "act_store_device": str(self.act_store_device),
+        }
+
+        return cfg_dict
+
+    def to_json(self, path: str) -> None:
+
+        if not os.path.exists(os.path.dirname(path)):
+            os.makedirs(os.path.dirname(path))
+
+        with open(path + "cfg.json", "w") as f:
+            json.dump(self.to_dict(), f, indent=2)
+
+    @classmethod
+    def from_json(cls, path: str) -> "LanguageModelSAERunnerConfig":
+        with open(path + "cfg.json", "r") as f:
+            cfg = json.load(f)
+        return cls(**cfg)
