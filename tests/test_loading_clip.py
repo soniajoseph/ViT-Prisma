6ca7a5e400b91ccbb24af15036160a60dc29b27c
lowlorenz <lorenz.hufe@hhi.fraunhofer.de>
2024-12-11 02:23:18 -0800

    ✅ Add openai clip-b-32 test

--- tests/test_loading_clip.py
+++ tests/test_loading_clip.py
@@ -7,16 +7,14 @@ import torch
 from transformers import CLIPProcessor, CLIPModel
 
 
-def test_loading_clip():
+def test_loading_clip(model_name):
     TOLERANCE = 1e-4
 
-    model_name = "wkcn/TinyCLIP-ViT-8M-16-Text-3M-YFCC15M"
     batch_size = 5
     channels = 3
     height = 224
     width = 224
     device = "cpu"
-
 
     full = CLIPModel.from_pretrained(model_name)
     tinyclip = full.vision_model
@@ -25,16 +23,24 @@ def test_loading_clip():
     tinyclip.to(device)
     tinyclip_final_proj.to(device)
 
-    hooked_model = HookedViT.from_pretrained(model_name, is_timm=False, is_clip=True)
+    hooked_model = HookedViT.from_pretrained(
+        model_name, is_timm=False, is_clip=True, fold_ln=False
+    )
     hooked_model.to(device)
 
     with torch.random.fork_rng():
         torch.manual_seed(1)
         input_image = torch.rand((batch_size, channels, height, width)).to(device)
     with torch.no_grad():
-        tinyclip_output, hooked_output = tinyclip_final_proj(tinyclip(input_image)[1]),  hooked_model(input_image)
+        tinyclip_output, hooked_output = tinyclip_final_proj(
+            tinyclip(input_image)[1]
+        ), hooked_model(input_image)
 
-    assert torch.allclose(hooked_output, tinyclip_output, atol=TOLERANCE), f"Model output diverges! Max diff: {torch.max(torch.abs(hooked_output - tinyclip_output))}"
-    print("Model output matches!")
+    assert torch.allclose(
+        hooked_output, tinyclip_output, atol=TOLERANCE
+    ), f"{model_name} output diverges! Max diff: {torch.max(torch.abs(hooked_output - tinyclip_output))}"
+    print(f"{model_name} output matches!")
 
-test_loading_clip()
+
+test_loading_clip("openai/clip-vit-base-patch32")
+test_loading_clip("wkcn/TinyCLIP-ViT-8M-16-Text-3M-YFCC15M")
