7cc0b2d8ef154679528a6fdab8415fb01bb3bc0e
soniajoseph <soniamollyjoseph@gmail.com>
2024-12-11 22:21:10 -0500

    add test for vit-bigg

--- tests/test_loading_CLIP-ViT-bigG-14-laion2B-39B-b160k.py
+++ tests/test_loading_CLIP-ViT-bigG-14-laion2B-39B-b160k.py
@@ -22,13 +22,64 @@ os.environ['OPEN_CLIP_CACHE'] = cache_dir
 os.environ['TRANSFORMERS_CACHE'] = cache_dir
 
 
-random_input = torch.rand((1, 3, 224, 224))
-hooked_vit_g = HookedViT.from_pretrained("laion/CLIP-ViT-bigG-14-laion2B-39B-b160k", is_clip=True, is_timm=False, fold_ln=False)
-hooked_vit_op = hooked_vit_g(random_input)
+
+og_model_name = "laion/CLIP-ViT-bigG-14-laion2B-39B-b160k"
+
+def test_output()
+
+    random_input = torch.rand((1, 3, 224, 224))
+    hooked_vit_g = HookedViT.from_pretrained(og_model_name, is_clip=True, is_timm=False, fold_ln=False)
+    hooked_vit_op = hooked_vit_g(random_input)
 
 
-hf_model = CLIPVisionModelWithProjection.from_pretrained("laion/CLIP-ViT-bigG-14-laion2B-39B-b160k", cache_dir=cache_dir)
-hf_model.eval()
-op = hf_model(random_input)
+    hf_model = CLIPVisionModelWithProjection.from_pretrained(og_model_name, cache_dir=cache_dir)
+    hf_model.eval()
+    op = hf_model(random_input)
 
-print(torch.allclose(hooked_vit_op, op.image_embeds, atol=1e-4))
+    print("Do does output match?")
+    print(torch.allclose(hooked_vit_op, op.image_embeds, atol=1e-4))
+
+def test_accuracy_baseline_og_model():
+    parent_dir = '/network/scratch/s/sonia.joseph/clip_benchmark/'
+    classifier = np.load(os.path.join(parent_dir, 'imagenet_classifier_hf_hub_laion_CLIP_ViT_bigG_14_laion2B_39B_b160k.npy'))
+
+    model_name = 'hf-hub:' + og_model_nam
+    og_model, _, preprocess = open_clip.create_model_and_transforms(model_name, cache_dir)
+    og_model.eval()
+
+    print("Classifier and model loaded")
+
+    data = {}
+    dataset_path =  "/network/scratch/s/sonia.joseph/datasets/kaggle_datasets"
+    data['imagenet-val'] = load_imagenet(preprocess_transform=preprocess, dataset_path=dataset_path, dataset_type='imagenet1k-val')
+    epoch = 1
+    results = zero_shot_eval(og_model, data, epoch, model_name=model_name, pretrained_classifier=classifier)
+    print("Results", results)
+    # I get 0.6918 on ImageNet Val; benchmarked in ML Foundations OpenCLIP repo is 0.6917
+
+
+def test_accuracy_baseline_hooked_model():
+    parent_dir = '/network/scratch/s/sonia.joseph/clip_benchmark/'
+    classifier = np.load(os.path.join(parent_dir, 'imagenet_classifier_hf_hub_laion_CLIP_ViT_bigG_14_laion2B_39B_b160k.npy'))
+
+    hooked_model = HookedViT.from_pretrained(og_model_name, is_timm=False, is_clip=True, fold_ln=False, center_writing_weights=False) # in future, do all models
+    hooked_model.to('cuda')
+    hooked_model.eval()
+
+    print("Classifier and model loaded")
+
+    model_name = 'hf-hub:' + og_model_name
+    og_model, _, preprocess = open_clip.create_model_and_transforms(model_name) # just need preprocessor
+    del og_model
+
+    print(preprocess)
+
+    data = {}
+    dataset_path =  "/network/scratch/s/sonia.joseph/datasets/kaggle_datasets"
+    data['imagenet-val'] = load_imagenet(preprocess_transform=preprocess, dataset_path=dataset_path, dataset_type='imagenet1k-val')
+    epoch = 1
+    results = zero_shot_eval(hooked_model, data, epoch, model_name=og_model_name, pretrained_classifier=classifier)
+    print("Results", results)
+    # I get 0.69178 on Hooked Model; benchmarked in ML Foundations OpenCLIP repo is 0.6917
+
+test_accuracy_baseline_hooked_model()
